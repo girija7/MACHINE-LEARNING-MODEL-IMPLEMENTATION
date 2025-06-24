be64d4e05565018e2f47fc691f0cde4c53191c6c
girija7 <girijagandham3669@gmail.com>
2025-06-24 21:59:14 +0530

    Create library.py

---
+++ library.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
